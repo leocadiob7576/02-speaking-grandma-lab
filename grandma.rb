f8aacec4ae9b74251e05097060b639338ef4005c
leocadiob7576 <leocadiob7576@hstat.org>
2017-10-12 15:57:36 +0000

    finish lab

---
+++ grandma.rb
@@ -0,0 +1,17 @@
+def speak_to_grandma(string)
+    if string == "I LOVE YOU GRANDMA!"
+        return  "I LOVE YOU TOO PUMPKIN!"
+    elsif
+        string == "Hi Nana, how are you?"
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif
+        string == "Hi!"
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif
+        string == "WHAT DID YOU EAT TODAY?"
+        return "NO, NOT SINCE 1938!"
+    elsif
+        string == "WHAT?"
+        return "NO, NOT SINCE 1938!"
+    end
+end
